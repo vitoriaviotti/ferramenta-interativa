04acc2a3daa573fee17f24df867cc87c1ab4cb7f
vitoriaviotti <137062011+vitoriaviotti@users.noreply.github.com>
2024-10-31 21:01:28 -0300

    s

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma linguagem de marcação.' 
+)
+
+criaCartao(
+    'Programação',
+    'O que é HEAD?',
+    'O HEAD é usado como cabeçalho.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é FOOTER?',
+    'O FOOTER é o conteúdo final de uma página.'    
+)
+
+criaCartao(
+    'Programação',
+    'O que é DIV?',
+    ' O DIV é um elemento HTML que serve como um conteiner.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é JAVASCRIPT?',
+    'O JAVASCRIPT é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização.'   
+) 
